7e6a610f9e7b02bdeed3739a91f716d4608ccdd3
SimoneFelici <qualcosa588@gmail.com>
2024-10-21 13:00:32 +0200

    some fixes

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <SDL2/SDL_video.h>
 #include <SDL2/SDL_image.h>
 
-
 int	main()
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -15,10 +14,9 @@ int	main()
 									SDL_WINDOWPOS_CENTERED,
 									SDL_WINDOWPOS_CENTERED,
 									800, 600, 0);
-
 	if (!window)
 	{
-		printf("Error creating window: %s\n", SDL_GetError());
+		printf("Error creating the window: %s\n", SDL_GetError());
 		SDL_Quit();
 		return (1);
 	}
@@ -28,12 +26,11 @@ int	main()
 									-1, render_flags);
 	if (!renderer)
 	{
-		printf("Error creating renderer: %s\n", SDL_GetError());
+		printf("Error creating the renderer: %s\n", SDL_GetError());
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return (1);
 	}
-
 	SDL_Surface *background = IMG_Load("resources/background.png");
 	if (!background)
 	{
@@ -43,25 +40,22 @@ int	main()
 		SDL_Quit();
 		return (1);
 	}
-
 	SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, background);
 	SDL_FreeSurface(background);
 	if (!texture)
 	{
-		printf("Error creating texture: %s\n", SDL_GetError());
+		printf("Error creating the texture: %s\n", SDL_GetError());
 		SDL_DestroyRenderer(renderer);
 		SDL_DestroyWindow(window);
 		SDL_Quit();
 		return (1);
 	}
-
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer,
 					texture,
 					NULL,
 					NULL);
 	SDL_RenderPresent(renderer);
-
 	SDL_Delay(3000);
 	SDL_DestroyTexture(texture);
 	SDL_DestroyRenderer(renderer);
